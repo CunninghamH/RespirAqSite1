c6a4c9d2a7a2bda62dfb6cabb2cb0ad3321fb8d1
CunninghamH <helen.cunningham@aut.ac.nz>
2021-10-05 13:40:39 +1300

    bug fixes

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -1,18 +1,20 @@
-  const navLinks = document.querySelectorAll('.nav-item')
+ 
+ 
+const navLinks = document.querySelectorAll('.nav-item')
   const menuToggle = document.getElementById('navbarNav')
   const bsCollapse = new bootstrap.Collapse(menuToggle)
 
   navLinks.forEach((l) => {
       l.addEventListener('click', () => { bsCollapse.toggle() })
-  })
+  });
 
 
   window.addEventListener("scroll", myFunction);
-  bsCollapse.hide();
+
   function myFunction() {
   
-if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) && (document.getElementById('navbarNav').height != 0)){
-    bsCollapse.hide()
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        bsCollapse.hide();
 
-} 
-}
+    }   
+}    
